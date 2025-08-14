d09e508625e2c4bab5f2416b86e7d4ebb949d246
quyenlt <lthoquyen2002@gmail.com>
2025-08-14 14:56:34 +0700

    Add Shopify authentication and session management: implement middleware for installation checks, session storage, and redirection logic

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,13 +3,25 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
+use Shopify\Utils;
+use App\Models\Session;
+use App\Lib\AuthRedirection;
 
-Route::get('/auth/callback', function (Request $request) {
-    $shop = $request->query('shop');
-    Log::info('Shop: ' . $shop);
+// Route::get('/auth/callback', function (Request $request) {
+//     $shop = $request->query('shop');
+//     Log::info('Shop: ' . $shop);
 
-    return response()->json([
-        'shop' => $shop,
-        'message' => 'Hello World',
-    ]);
+//     return response()->json([
+//         'shop' => $shop,
+//         'message' => 'Hello World',
+//     ]);
+// });
+
+Route::get('/auth', function (Request $request) {
+    $shop = Utils::sanitizeShopDomain($request->query('shop'));
+
+    // Delete any previously created OAuth sessions that were not completed (don't have an access token)
+    Session::where('shop', $shop)->where('access_token', null)->delete();
+
+    return AuthRedirection::redirect($request);
 });
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,24 +1,24 @@
 <?php
 
-use Illuminate\Support\Facades\Request;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Shopify\Context;
 use Shopify\Utils;
 
-// Route::fallback(function (Request $request) {
-//     if (Context::$IS_EMBEDDED_APP &&  $request->query("embedded", false) === "1") {
-//         if (env('APP_ENV') === 'production') {
-//             return file_get_contents(public_path('index.html'));
-//         } else {
-//             return file_get_contents(base_path('frontend/index.html'));
-//         }
-//     } else {
-//         return redirect(Utils::getEmbeddedAppUrl($request->query("host", null)) . "/" . $request->path());
-//     }
-// })->middleware('shopify.installed');
+Route::fallback(function (Request $request) {
+    if (Context::$IS_EMBEDDED_APP &&  $request->query("embedded", false) === "1") {
+        if (env('APP_ENV') === 'production') {
+            return redirect('https://b2b.qoutesnap.local/frontend/');
+        } else {
+            return redirect('https://b2b.qoutesnap.local/frontend/');
+        }
+    } else {
+        return redirect(Utils::getEmbeddedAppUrl($request->query("host", null)) . "/" . $request->path());
+    }
+})->middleware('shopify.installed');
 
-Route::get('/', function () {
-    return response()->json([
-        'message' => 'Welcome to the Shopify App API',
-    ]);
-});
+// Route::get('/', function () {
+//     return response()->json([
+//         'message' => 'Welcome to the Shopify App API',
+//     ]);
+// });
